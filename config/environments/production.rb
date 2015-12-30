31affc9293d3964b7ad605178133bd19dd51bcd4
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2015-12-30 10:12:20 -0200

    precopile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -88,7 +88,7 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
   config.assets.paths << Rails.root.join("app","assets","fonts","audio")
-  
+
 
 
     #set perpeclip opload  imeans to amazon s3
