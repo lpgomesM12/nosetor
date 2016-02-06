b736bb13a17d35baa2a299b907ef6d998972adff
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-02-05 22:07:29 -0200

    alteracao deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 
 set :application, 'nosetor'
 set :repo_url, 'git@github.com:lpgomesM12/nosetor.git'
-
+set :scm, :git
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -11,7 +11,7 @@ set :repo_url, 'git@github.com:lpgomesM12/nosetor.git'
 set :deploy_to, '/home/deploy/nosetor'
 
 # Default value for :scm is :git
-# set :scm, :git
+
 
 # Default value for :format is :pretty
 # set :format, :pretty
